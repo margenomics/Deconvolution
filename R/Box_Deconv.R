0d1607238b37d780080eeb40022ce8c1d82c3174
Nidia Barco <nidiabarco16@gmail.com>
2022-03-10 11:54:56 +0100

    Actualization

--- R/Box_Deconv.R
+++ R/Box_Deconv.R
@@ -16,8 +16,6 @@
 ##' @examples
 ##' Box_Deconv(data= C_abs, cond= fractions, results_dir = "/bicoh/nidia/Deconv/Brain_MMartin/test", f_name= "Box_CIBERSORT_abs")
 
-my_comparisons <- list( c("A", "B"), c("B", "C"), c("A", "C") )
-
 Box_Deconv <- function(data, cond, results_dir, f_name= "Deconvolution Boxplot"){
 
   require(ggplot2)
@@ -27,15 +25,19 @@ Box_Deconv <- function(data, cond, results_dir, f_name= "Deconvolution Boxplot")
   require(ggsignif)
   require(tidyverse)
 
+  # Manipulació del df de deconvolució, perque tingui l'estructura per generar els boxplots
   data$cell_type <- NULL
   data <- t(data)
   test_m <- melt(as.data.table(data), variable.factor = FALSE)
   condition <- c(rep(cond, times=length(colnames(data))))
   df_m <- cbind(test_m, condition)
 
+  # Assignació del nom per al gràfic, per defecte "Deconvolution Boxplot"
   file <- c(f_name, ".png")
   file <- paste(file, collapse = "")
 
+  # Si hi ha dues condicions es genera el grafic partit per cell type i amb el P_value per condició i apareix en cada caixa
+  # Si hi ha mes de dues condicions es genera el grafic partit per cell type sense els P_value, els P_value per cada cell type surten com a missatges, i retorna un df amb info dels P_values i els nivells de significancia per condició
   if (length(unique(cond)) == 2){
     p <- ggplot(data = df_m, aes(x=variable, y=value)) +
       geom_boxplot(aes(fill=condition)) + stat_compare_means(aes(group=condition), method = "t.test")
